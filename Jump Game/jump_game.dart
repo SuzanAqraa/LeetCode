18afcbf962609b426c0081673258c72f9e8a6fe2
SuzanAqraa <s12217677@stu.najah.edu>
2025-08-05 19:33:08 +0300

    Add solution for Jump Game

---
+++ Jump Game/jump_game.dart
@@ -0,0 +1,10 @@
+class Solution {
+  bool canJump(List<int> nums) {
+    int maxReach = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (i > maxReach) return false; 
+      maxReach = maxReach > i + nums[i] ? maxReach : i + nums[i];
+    }
+    return true;
+  }
+}
